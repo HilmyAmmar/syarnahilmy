537e6fb2c133b1a5fc43ee40a524763b51198e69
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2025-07-06 05:02:27 +0700

    Add more data

--- src/app/component/AboutHer.tsx
+++ src/app/component/AboutHer.tsx
@@ -7,9 +7,6 @@ export default function AboutHer() {
     <div className="bg-white max-w-2xl shadow overflow-hidden sm:rounded-lg mb-8">
       <div className="px-4 py-5 sm:px-6">
         <h3 className="text-lg leading-6 font-medium text-gray-900">About Her</h3>
-        {/* <p className="mt-1 max-w-2xl text-sm text-gray-500">
-          What makes her special to me? 💖
-        </p> */}
       </div>
       <div className="border-t border-gray-200">
         <dl>
@@ -17,6 +14,12 @@ export default function AboutHer() {
             <dt className="text-sm font-medium text-gray-500">Full Name</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
               Syarna Savitri
+            </dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Nickname</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              Bulat, Kebab, Adik, Cantik mbiw 
             </dd>
           </div>
           <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -53,6 +56,18 @@ export default function AboutHer() {
             <dt className="text-sm font-medium text-gray-500">What's I feel about her?</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                 I'm so grateful to have her in my life. In god of war, there's this statistics called 'Luck', i feel like my LUCK is so high that the number didn't even fit the screen. I'm like the luckiest PERSON ever to be her boyfriend. I love her so much, in the past, right now, in the future, and forever. I can't see my life, my future without her, she is everything to me. she is not only part of my soul, but she is my whole soul. 
+            </dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">What's have i learned from her?</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                I learned to be more romantic and know how to treat her right. I learned a lot about real-life skills from her too. She taught me how to ride a motorcycle and drive a car. She told me do and don'ts when you around other people and socializing with them. She's the best lah pokoknye.
+            </dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">What I hope for our future?</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                I hope we always be together forever ever. We'll be having fun forever, never get bored with each other, always be there for each other. I hope we can travel to UK, europe, and every country we have planned to. I hope we can marry 2-3 years after i graduate and she get the best ring, best wedding dress, and best wedding overall. I hope we'll have a happy family until old age. WE RIDE TOGETHER WE DIE TOGETHER. 
             </dd>
           </div>
         </dl>
